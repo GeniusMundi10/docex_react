f2c60b5630f8f032b27503e705a36b56f678a95a
GeniusMundi10 <159649324+GeniusMundi10@users.noreply.github.com>
2024-07-09 14:42:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,4 +67,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const re = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
         return re.test(String(email).toLowerCase());
     }
+
+    // Initialize Slick Carousel
+    $('.feature-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 });
