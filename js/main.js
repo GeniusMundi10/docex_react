9523b621ab8d4e8fbd09aa1d5616a12d6d8576c0
GeniusMundi10 <159649324+GeniusMundi10@users.noreply.github.com>
2024-07-11 03:07:11 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,29 +10,14 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
     });
 
-    // Get the modal
-    var modal = document.getElementById('demoModal');
+    // Handling the modal opening
+    $('#open-modal').click(function(e) {
+        e.preventDefault();
+        $('.modal-backdrop').show();
+    });
 
-    // Get the button that opens the modal
-    var btn = document.querySelector('.demo-btn');
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+    // Handling the modal closing
+    $('.close').click(function() {
+        $('.modal-backdrop').hide();
+    });
 });
