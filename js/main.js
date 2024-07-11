88e0283bbb35cc6b168a50e0ab654714aa12beb8
GeniusMundi10 <159649324+GeniusMundi10@users.noreply.github.com>
2024-07-11 02:55:28 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-    $('.feature-slider').slick({
+$(document).ready(function(){
+    // Initialize Slick sliders
+    $('.feature-slider, .steps-slider').slick({
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
     });
+
+    // Get the modal
+    var modal = document.getElementById('demoModal');
+
+    // Get the button that opens the modal
+    var btn = document.querySelector('.demo-btn');
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 });
