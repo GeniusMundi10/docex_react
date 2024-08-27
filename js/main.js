289d6e2389642a1469051668d8dbafc53fd1515a
GeniusMundi10 <159649324+GeniusMundi10@users.noreply.github.com>
2024-08-27 21:46:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 1000,
         adaptiveHeight: true
     });
 
